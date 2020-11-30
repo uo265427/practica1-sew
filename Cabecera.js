6bc84bea807d4341adfe3710436ecfa72c6d9caf
maria grande <71706519+uo265427@users.noreply.github.com>
2020-11-30 18:21:59 +0100

    Add files via upload

---
+++ Cabecera.js
@@ -0,0 +1,8 @@
+var asignatura = new Object();
+    asignatura.nombre = "Software y estandares en la Web";
+    asignatura.titulacion = "Grado en Ingenieri­a Inforatica del Software";
+    asignatura.centro = "Escuela de Ingenieri­a Informatica";
+    asignatura.universidad = "Universidad de Oviedo";
+    asignatura.curso = "2020-21";
+    asignatura.alumno = "Maria Luisa Grande Junquera";
+    asignatura.email = "uo265427@uniovi.es";
